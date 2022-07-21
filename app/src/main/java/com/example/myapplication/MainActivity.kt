03515780d2787822a8b49debb2bce82e701688ab
malyq <maxim99112@gmail.com>
2022-07-21 16:48:52 +0500

    massive

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       /* val name = findViewById<EditText>(R.id.myname).text.toString()
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            println("Ваше имя:  $name")
+        }*/
+        val array: List<Int> = listOf(1,2,3,4,5)
+        for(i in 1..5){
+            for(j in 1..5){
+                print("$i")
+            }
+            println()
+        }
     }
 }
